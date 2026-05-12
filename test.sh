642d83936c885365118b2ac5243d0de0d05a1036
Nick Burt <nbur4556@gmail.com>
2026-05-11 20:28:14 -0500

    run test script as python unit tests

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 export PYTHONPATH=$PYTHONPATH.
-"$SCRIPT_DIR/.venv/bin/python3.13" "$SCRIPT_DIR/tests/test_etl.py"
+"$SCRIPT_DIR/.venv/bin/python3.13" -m unittest "$SCRIPT_DIR/tests/test_etl.py"
